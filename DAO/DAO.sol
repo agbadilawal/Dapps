d7580eb922515684201a3d7fb3a55849a34789de
sumGodbro <64108848+agbadilawal@users.noreply.github.com>
2022-06-17 09:05:15 +0100

    Update DAO.sol

--- DAO/DAO.sol
+++ DAO/DAO.sol
@@ -1,27 +1,46 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.0;
 
+//! import and use OZ safemath & reentrancy guard libraries
+
 contract DAO {
-  mapping (address=>bool) public investors;
-  mapping (address=>uint) public shares;
-  uint public totalShares;
-  uint public availableFunds;
-  uint public contributionEnd;
+    mapping(address => bool) public investors;
+    mapping(address => uint256) public shares; //* make the shares ERC20 tokens so it can use the erc20 functions like transfer
+    uint256 public totalShares;
+    uint256 public availableFunds;
+    uint256 public contributionEnd;
 
-  constructor(uint contributionTime){
-    contributionEnd = block.timestamp + contributionTime;
-  }
+    constructor(uint256 contributionTime) {
+        contributionEnd = block.timestamp + contributionTime;
+    }
 
-  function contribute() payable external{
-    require(block.timestamp < contributionEnd,"cant contribute after contribution time elapse");
-    investors[msg.sender] += true;
-    shares[msg.sender] += msg.value;
-    totalShares += msg.value;
-    availableFunds += msg.value;
-  }
+    function contribute() external payable {
+        require(
+            block.timestamp < contributionEnd,
+            "cant contribute after contribution time elapse"
+        );
+        investors[msg.sender] += true;
+        shares[msg.sender] += msg.value;
+        totalShares += msg.value;
+        availableFunds += msg.value;
+    }
 
-  function redeemShares(uint amount) {
-    require(shares[msg.sender] >= amount, "insufficient balance");
-    require(investors[msg.sender]);
-  }
-} 
+    function redeemShares(uint256 amount) {
+        require(shares[msg.sender] >= amount, "insufficient shares");
+        require(investors[msg.sender], "you have to be an investor");
+        require(availableFunds >= amount, "insufficient funds in the pool");
+        shares[msg.sender] -= amount;
+        availableFunds -= amount;
+        msg.sender.transfer(amount); // this sends the balance to the investors wallet
+    }
+    //! set if investors share balance is empty, remove it from investor mapping
+
+    function tranferShares(uint256 amount, address payable recipient) external {
+        require(shares[msg.sender] >= amount, "insufficient shares");
+        require(investors[msg.sender], "you have to be an investor");
+
+        shares[msg.sender] -= amount;
+        investors[recipient] = true;
+        shares[recipient] += amount;        
+    }
+}
